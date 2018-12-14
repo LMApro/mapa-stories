cc2054fd5ad20a770dd29df50b179ed8bc4ceb95
makatz <matn95@gmail.com>
2018-12-14 19:30:23 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          
+        </header>
+        <main>
+          
+        </main>
+        <footer>
+
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default App;
