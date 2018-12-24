1e0541e0d9b15cb4e2784322d94671e0fbca5e5a
makatz <matn95@gmail.com>
2018-12-24 23:15:18 +0700

    Update imgs

--- js/images.js
+++ js/images.js
@@ -71,5 +71,281 @@ window.images = [
         "width": 1080,
         "height": 1618,
         "ratio": 1.4981481481481482
+    },
+    {
+        "filename": "AAA_1299-min.jpg",
+        "width": 1080,
+        "height": 721,
+        "ratio": 0.6675925925925926
+    },
+    {
+        "filename": "AAA_1331-min.jpg",
+        "width": 1080,
+        "height": 721,
+        "ratio": 0.6675925925925926
+    },
+    {
+        "filename": "AAA_1336-min.jpg",
+        "width": 1080,
+        "height": 721,
+        "ratio": 0.6675925925925926
+    },
+    {
+        "filename": "AAA_1365-min.jpg",
+        "width": 1080,
+        "height": 721,
+        "ratio": 0.6675925925925926
+    },
+    {
+        "filename": "AAA_1373-min.jpg",
+        "width": 1080,
+        "height": 721,
+        "ratio": 0.6675925925925926
+    },
+    {
+        "filename": "AAA_1387-min.jpg",
+        "width": 1080,
+        "height": 1618,
+        "ratio": 1.4981481481481482
+    },
+    {
+        "filename": "AAA_1468-min.jpg",
+        "width": 1080,
+        "height": 1618,
+        "ratio": 1.4981481481481482
+    },
+    {
+        "filename": "AAA_1484-min.jpg",
+        "width": 1080,
+        "height": 721,
+        "ratio": 0.6675925925925926
+    },
+    {
+        "filename": "AAA_1519-min.jpg",
+        "width": 1080,
+        "height": 1618,
+        "ratio": 1.4981481481481482
+    },
+    {
+        "filename": "AAA_1550-min.jpg",
+        "width": 1080,
+        "height": 721,
+        "ratio": 0.6675925925925926
+    },
+    {
+        "filename": "AAA_1580-min.jpg",
+        "width": 1080,
+        "height": 1618,
+        "ratio": 1.4981481481481482
+    },
+    {
+        "filename": "NHU_3993-min.jpg",
+        "width": 1080,
+        "height": 1618,
+        "ratio": 1.4981481481481482
+    },
+    {
+        "filename": "NHU_4035-min.jpg",
+        "width": 1080,
+        "height": 721,
+        "ratio": 0.6675925925925926
+    },
+    {
+        "filename": "NHU_4038-min.jpg",
+        "width": 1080,
+        "height": 1618,
+        "ratio": 1.4981481481481482
+    },
+    {
+        "filename": "NHU_4069-min.jpg",
+        "width": 1080,
+        "height": 721,
+        "ratio": 0.6675925925925926
+    },
+    {
+        "filename": "NHU_4079-min.jpg",
+        "width": 1080,
+        "height": 721,
+        "ratio": 0.6675925925925926
+    },
+    {
+        "filename": "NHU_4091-min.jpg",
+        "width": 1080,
+        "height": 721,
+        "ratio": 0.6675925925925926
+    },
+    {
+        "filename": "NHU_4110-min.jpg",
+        "width": 1080,
+        "height": 721,
+        "ratio": 0.6675925925925926
+    },
+    {
+        "filename": "NHU_4152-min.jpg",
+        "width": 1080,
+        "height": 721,
+        "ratio": 0.6675925925925926
+    },
+    {
+        "filename": "NHU_4183-min.jpg",
+        "width": 1080,
+        "height": 721,
+        "ratio": 0.6675925925925926
+    },
+    {
+        "filename": "NHU_4190-min.jpg",
+        "width": 1080,
+        "height": 1618,
+        "ratio": 1.4981481481481482
+    },
+    {
+        "filename": "NHU_4205-min.jpg",
+        "width": 1080,
+        "height": 1618,
+        "ratio": 1.4981481481481482
+    },
+    {
+        "filename": "NHU_4209-min.jpg",
+        "width": 1080,
+        "height": 1618,
+        "ratio": 1.4981481481481482
+    },
+    {
+        "filename": "NHU_4255-min.jpg",
+        "width": 1080,
+        "height": 721,
+        "ratio": 0.6675925925925926
+    },
+    {
+        "filename": "NHU_4263-min.jpg",
+        "width": 1080,
+        "height": 721,
+        "ratio": 0.6675925925925926
+    },
+    {
+        "filename": "NHU_4306-min.jpg",
+        "width": 1080,
+        "height": 721,
+        "ratio": 0.6675925925925926
+    },
+    {
+        "filename": "NHU_4317-min.jpg",
+        "width": 1080,
+        "height": 1618,
+        "ratio": 1.4981481481481482
+    },
+    {
+        "filename": "NHU_4324-min.jpg",
+        "width": 1080,
+        "height": 721,
+        "ratio": 0.6675925925925926
+    },
+    {
+        "filename": "NHU_4364-min.jpg",
+        "width": 1080,
+        "height": 1618,
+        "ratio": 1.4981481481481482
+    },
+    {
+        "filename": "NHU_4421-min.jpg",
+        "width": 1080,
+        "height": 721,
+        "ratio": 0.6675925925925926
+    },
+    {
+        "filename": "NHU_4487-min.jpg",
+        "width": 1080,
+        "height": 721,
+        "ratio": 0.6675925925925926
+    },
+    {
+        "filename": "NHU_4519-min.jpg",
+        "width": 1080,
+        "height": 721,
+        "ratio": 0.6675925925925926
+    },
+    {
+        "filename": "NHU_4524-min.jpg",
+        "width": 1080,
+        "height": 721,
+        "ratio": 0.6675925925925926
+    },
+    {
+        "filename": "NHU_4530-min.jpg",
+        "width": 1080,
+        "height": 1618,
+        "ratio": 1.4981481481481482
+    },
+    {
+        "filename": "NHU_4546-min.jpg",
+        "width": 1080,
+        "height": 721,
+        "ratio": 0.6675925925925926
+    },
+    {
+        "filename": "NHU_4574-min.jpg",
+        "width": 1080,
+        "height": 1618,
+        "ratio": 1.4981481481481482
+    },
+    {
+        "filename": "NHU_4598-min.jpg",
+        "width": 1080,
+        "height": 1618,
+        "ratio": 1.4981481481481482
+    },
+    {
+        "filename": "NHU_4606-min.jpg",
+        "width": 1080,
+        "height": 721,
+        "ratio": 0.6675925925925926
+    },
+    {
+        "filename": "NHU_4643-min.jpg",
+        "width": 1080,
+        "height": 1618,
+        "ratio": 1.4981481481481482
+    },
+    {
+        "filename": "NHU_4729-min.jpg",
+        "width": 1080,
+        "height": 721,
+        "ratio": 0.6675925925925926
+    },
+    {
+        "filename": "NHU_4758-min.jpg",
+        "width": 1080,
+        "height": 1618,
+        "ratio": 1.4981481481481482
+    },
+    {
+        "filename": "NHU_4759-min.jpg",
+        "width": 1080,
+        "height": 1618,
+        "ratio": 1.4981481481481482
+    },
+    {
+        "filename": "NHU_4780-min.jpg",
+        "width": 1080,
+        "height": 1618,
+        "ratio": 1.4981481481481482
+    },
+    {
+        "filename": "NHU_4798-min.jpg",
+        "width": 1080,
+        "height": 721,
+        "ratio": 0.6675925925925926
+    },
+    {
+        "filename": "NHU_4826-min.jpg",
+        "width": 1080,
+        "height": 1618,
+        "ratio": 1.4981481481481482
+    },
+    {
+        "filename": "NHU_4850-min.jpg",
+        "width": 1080,
+        "height": 721,
+        "ratio": 0.6675925925925926
     }
 ]
