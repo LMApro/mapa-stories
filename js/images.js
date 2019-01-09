10e10f749e106881c89c5c3642b97d1de4711521
makatz <matn95@gmail.com>
2019-01-09 23:18:43 +0700

    Update img

--- js/images.js
+++ js/images.js
@@ -43,16 +43,238 @@ window.images = [
         "ratio": 0.75
     },
     {
+        "filename": "009-min.jpg",
+        "width": 1080,
+        "height": 1920,
+        "ratio": 1.7777777777777777
+    },
+    {
+        "filename": "010-min.jpg",
+        "width": 1080,
+        "height": 1819,
+        "ratio": 1.6842592592592593
+    },
+    {
+        "filename": "011-min.jpg",
+        "width": 1080,
+        "height": 1920,
+        "ratio": 1.7777777777777777
+    },
+    {
+        "filename": "012-min.jpg",
+        "width": 1080,
+        "height": 1920,
+        "ratio": 1.7777777777777777
+    },
+    {
+        "filename": "013-min.jpg",
+        "width": 1080,
+        "height": 608,
+        "ratio": 0.562962962962963
+    },
+    {
+        "filename": "014-min.jpg",
+        "width": 1080,
+        "height": 1350,
+        "ratio": 1.25
+    },
+    {
+        "filename": "015-min.jpg",
+        "width": 1080,
+        "height": 830,
+        "ratio": 0.7685185185185185
+    },
+    {
+        "filename": "016-min.jpg",
+        "width": 1080,
+        "height": 810,
+        "ratio": 0.75
+    },
+    {
+        "filename": "017-min.jpg",
+        "width": 1080,
+        "height": 1440,
+        "ratio": 1.3333333333333333
+    },
+    {
+        "filename": "018-min.jpg",
+        "width": 1080,
+        "height": 1440,
+        "ratio": 1.3333333333333333
+    },
+    {
+        "filename": "019-min.jpg",
+        "width": 1080,
+        "height": 1440,
+        "ratio": 1.3333333333333333
+    },
+    {
         "filename": "020-min.jpg",
         "width": 1080,
         "height": 1350,
         "ratio": 1.25
     },
     {
+        "filename": "021-min.jpg",
+        "width": 1080,
+        "height": 1622,
+        "ratio": 1.5018518518518518
+    },
+    {
+        "filename": "022-min.jpg",
+        "width": 1080,
+        "height": 1623,
+        "ratio": 1.5027777777777778
+    },
+    {
+        "filename": "023-min.jpg",
+        "width": 1080,
+        "height": 1623,
+        "ratio": 1.5027777777777778
+    },
+    {
+        "filename": "024-min.jpg",
+        "width": 1080,
+        "height": 719,
+        "ratio": 0.6657407407407407
+    },
+    {
+        "filename": "025-min.jpg",
+        "width": 1080,
+        "height": 1080,
+        "ratio": 1
+    },
+    {
         "filename": "026-min.jpg",
         "width": 1080,
         "height": 1080,
         "ratio": 1
+    },
+    {
+        "filename": "027-min.jpg",
+        "width": 1080,
+        "height": 1080,
+        "ratio": 1
+    },
+    {
+        "filename": "028-min.jpg",
+        "width": 1080,
+        "height": 1113,
+        "ratio": 1.0305555555555554
+    },
+    {
+        "filename": "029-min.jpg",
+        "width": 1080,
+        "height": 1080,
+        "ratio": 1
+    },
+    {
+        "filename": "030-min.jpg",
+        "width": 1080,
+        "height": 608,
+        "ratio": 0.562962962962963
+    },
+    {
+        "filename": "031-min.jpg",
+        "width": 1080,
+        "height": 1440,
+        "ratio": 1.3333333333333333
+    },
+    {
+        "filename": "032-min.jpg",
+        "width": 1080,
+        "height": 1399,
+        "ratio": 1.2953703703703703
+    },
+    {
+        "filename": "033-min.jpg",
+        "width": 1080,
+        "height": 810,
+        "ratio": 0.75
+    },
+    {
+        "filename": "034-min.jpg",
+        "width": 1080,
+        "height": 1440,
+        "ratio": 1.3333333333333333
+    },
+    {
+        "filename": "035-min.jpg",
+        "width": 1080,
+        "height": 770,
+        "ratio": 0.7129629629629629
+    },
+    {
+        "filename": "036-min.jpg",
+        "width": 1080,
+        "height": 1440,
+        "ratio": 1.3333333333333333
+    },
+    {
+        "filename": "037-min.jpg",
+        "width": 1080,
+        "height": 1440,
+        "ratio": 1.3333333333333333
+    },
+    {
+        "filename": "038-min.jpg",
+        "width": 1080,
+        "height": 1440,
+        "ratio": 1.3333333333333333
+    },
+    {
+        "filename": "039-min.jpg",
+        "width": 1080,
+        "height": 1440,
+        "ratio": 1.3333333333333333
+    },
+    {
+        "filename": "040-min.jpg",
+        "width": 1080,
+        "height": 810,
+        "ratio": 0.75
+    },
+    {
+        "filename": "041-min.jpg",
+        "width": 1080,
+        "height": 1440,
+        "ratio": 1.3333333333333333
+    },
+    {
+        "filename": "042-min.jpg",
+        "width": 1080,
+        "height": 734,
+        "ratio": 0.6796296296296296
+    },
+    {
+        "filename": "043-min.jpg",
+        "width": 1080,
+        "height": 1440,
+        "ratio": 1.3333333333333333
+    },
+    {
+        "filename": "044-min.jpg",
+        "width": 1080,
+        "height": 720,
+        "ratio": 0.6666666666666666
+    },
+    {
+        "filename": "045-min.jpg",
+        "width": 1080,
+        "height": 1440,
+        "ratio": 1.3333333333333333
+    },
+    {
+        "filename": "046-min.jpg",
+        "width": 1080,
+        "height": 810,
+        "ratio": 0.75
+    },
+    {
+        "filename": "047-min.jpg",
+        "width": 1080,
+        "height": 1440,
+        "ratio": 1.3333333333333333
     },
     {
         "filename": "048-min.jpg",
@@ -317,5 +539,23 @@ window.images = [
         "width": 1080,
         "height": 721,
         "ratio": 0.6675925925925926
+    },
+    {
+        "filename": "096-min.jpg",
+        "width": 1080,
+        "height": 1619,
+        "ratio": 1.499074074074074
+    },
+    {
+        "filename": "097-min.jpg",
+        "width": 1080,
+        "height": 720,
+        "ratio": 0.6666666666666666
+    },
+    {
+        "filename": "098-min.jpg",
+        "width": 1080,
+        "height": 1080,
+        "ratio": 1
     }
 ]
